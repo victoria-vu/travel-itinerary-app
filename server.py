153e5f096289fd314f77b219bfd19143f215afaf
Victoria Vu <victoriamvvu@gmail.com>
2023-01-24 17:42:36 -0800

    Modified server.py, base.html, homepage.html and created log in and sign up for an account page

--- server.py
+++ server.py
@@ -4,15 +4,17 @@ Provides web interface for creating travel itinerary, opening a to-do-list,
 and adding locations to a map."""
 
 
-from flask import Flask, render_template, redirect, flash
-import jinja2
+from flask import Flask, render_template, request, flash, session, redirect
+from model import connect_to_db, db
+import crud
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 app.secret_key = "dev"
 
 # If you refer to undefined variable in a Jinja template, Jinja will ignore it.
 # We need to set an attribute of the Jinja environment that says to make this an error.
-app.jinja_env.undefined = jinja2.StrictUndefined
+app.jinja_env.undefined = StrictUndefined
 
 
 @app.route("/")
@@ -20,6 +22,13 @@ def homepage():
     """View homepage."""
 
     return render_template("homepage.html")
+
+
+@app.route("/log-in")
+def log_in():
+    """Show user log-in page."""
+    
+    return render_template("log_in.html")
 
 
 if __name__ == "__main__":
