e3e7e0abc74b385dfb92c386f3b567135f861e07
Victoria Vu <victoriamvvu@gmail.com>
2023-01-24 23:21:31 -0800

    Modified server.py, base.html, homepage.html and created mystyles.css

--- server.py
+++ server.py
@@ -18,18 +18,26 @@ app.jinja_env.undefined = StrictUndefined
 
 
 @app.route("/")
-def homepage():
-    """View homepage."""
+def show_homepage():
+    """Shows the homepage."""
+
+    # Code here: Need to write a code to define session user to pass to Jinja.
 
     return render_template("homepage.html")
 
 
 @app.route("/log-in")
-def log_in():
-    """Show user log-in page."""
+def show_login_page():
+    """Shows user the log-in page."""
     
     return render_template("log_in.html")
 
+
+@app.route("/sign-up")
+def show_signup_page():
+    """Shows user the sign-up page."""
+
+    return render_template("create_account.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
